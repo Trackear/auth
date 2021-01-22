c2927f9324de9a39775ba08b39de29c674dd9bdb
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-22 11:19:55 -0300

    Always rebuild on deploy

--- config/prod.exs
+++ config/prod.exs
@@ -12,8 +12,7 @@ use Mix.Config
 config :trackear_auth, TrackearAuthWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: {:system, "WEB_HOST"}, port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
