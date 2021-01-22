5540ab40c5337b423a26556433744679033cb7c8
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-01-22 02:45:31 -0300

    Trying release mode

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -36,3 +36,4 @@ config :trackear_auth, TrackearAuthWeb.Endpoint,
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
+config :trackear_auth, TrackearAuthWeb.Endpoint, server: true
